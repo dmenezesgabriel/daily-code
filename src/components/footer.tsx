6043c2e606f4643f547a70a9b814aa9281618d16
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-02-08 11:45:22 -0300

    chore: update footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,30 +1,37 @@
+import Link from "next/link";
+
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="cartoon-border relative overflow-hidden bg-yellow-300 p-6 text-gray-900">
       <div className="halftone-bg absolute inset-0" />
       <div className="container relative z-10 mx-auto text-center">
         <p className="text-xl">
-          &copy; 2023 The Daily Scoop. All scoops reserved.
+          &copy; {year} The Daily Scoop. All scoops reserved.
         </p>
         <div className="mt-4 flex justify-center space-x-4">
-          <a
+          <Link
             href="#"
+            target="_blank"
+            className="text-lg text-red-600 transition-colors hover:text-red-800"
+          >
+            Github
+          </Link>
+          <Link
+            href="#"
+            target="_blank"
+            className="text-lg text-red-600 transition-colors hover:text-red-800"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href="#"
+            target="_blank"
             className="text-lg text-red-600 transition-colors hover:text-red-800"
           >
             Twitter
-          </a>
-          <a
-            href="#"
-            className="text-lg text-red-600 transition-colors hover:text-red-800"
-          >
-            Facebook
-          </a>
-          <a
-            href="#"
-            className="text-lg text-red-600 transition-colors hover:text-red-800"
-          >
-            Instagram
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
